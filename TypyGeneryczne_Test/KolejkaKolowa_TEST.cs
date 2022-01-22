eaca6a84e90c8ef9450f869491b13bb13c141a68
Vladmir-JJ <jjwysocki89@gmail.com>
2022-01-22 16:29:37 +0100

    1_5 TestyJednostkowe

---
+++ TypyGeneryczne_Test/KolejkaKolowa_TEST.cs
@@ -0,0 +1,56 @@
+﻿using _1_TypyGeneryczne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TypyGeneryczne_Test
+{
+    [TestClass]
+    public class KolejkaKolowa_TEST
+    {
+        [TestMethod]
+        public void NowaKolejkaJestPusta()
+        {
+            var kolejka = new KolejkaKolowa();
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+        [TestMethod]
+        public void KolejkaTrzyelementowaJestPelnaPoTrzechZapisach()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            kolejka.Zapisz(3.6);
+            kolejka.Zapisz(1.2);
+            kolejka.Zapisz(11);
+            Assert.IsTrue(kolejka.JestPelny);
+        }
+        [TestMethod]
+        public void PierwszyWchodziPierwszyWychodzi()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosc1 = 4.6;
+            var wartosc2 = 3.7;
+
+            kolejka.Zapisz(wartosc1);
+            kolejka.Zapisz(wartosc2);
+
+            Assert.AreEqual(wartosc1, kolejka.Czytaj());
+            Assert.AreEqual(wartosc2, kolejka.Czytaj());
+
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+        [TestMethod]
+        public void NadpisujeGdyJestWiekszaNizPojemnosc()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosci = new[] { 1.2, 2.4, 3.6, 4.8, 3, 2};
+            foreach(var wartosc in wartosci)
+            {
+                kolejka.Zapisz(wartosc);
+            }
+            Assert.IsTrue(kolejka.JestPelny);
+            Assert.AreEqual(wartosci[3], kolejka.Czytaj());
+            Assert.AreEqual(wartosci[4], kolejka.Czytaj());
+            Assert.AreEqual(wartosci[5], kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+    }
+}
---
+++ TypyGeneryczne_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TypyGeneryczne_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TypyGeneryczne_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("021042f8-8d2d-4074-a791-d6361cc9801b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
