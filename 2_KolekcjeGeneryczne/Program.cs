204133a5d8f79f5709f34a21d1e2bc5ba52f14bb
Vladmir-JJ <jjwysocki89@gmail.com>
2022-01-25 10:53:24 +0100

    Dictionary, Sorted Dic, Tests

--- 2_KolekcjeGeneryczne/Program.cs
+++ 2_KolekcjeGeneryczne/Program.cs
@@ -11,25 +11,26 @@ namespace _2_KolekcjeGeneryczne
             //Stos();
             //HashSet();
             //LinkedList();
+            //LinkedList2();
+            //Slownik();
+            //Slownik2();
 
-            LinkedList<int> lista = new LinkedList<int>();
-            lista.AddFirst(5);
-            lista.AddFirst(6);
-            lista.AddFirst(7);
+            var pracownicy = new SortedDictionary<string, List<Pracownik>>();
+            pracownicy.Add("Sprzedaż", new List<Pracownik>() { new Pracownik { Imie = "Jan", Nazwisko = "Kowalski"},
+                                                                            new Pracownik { Imie = "Kazimierz", Nazwisko = "Beton"},
+                                                                             new Pracownik { Imie = "Urszula", Nazwisko = "Krzak"} });
 
-            var element1 = lista.First;
-            var elementOstatni = lista.Last;
-            lista.AddAfter(element1, 10);
-            lista.AddBefore(element1, 20);
+            pracownicy.Add("Informatyka", new List<Pracownik>() { new Pracownik { Imie = "Marcin", Nazwisko = "Kowal"},
+                                                                            new Pracownik { Imie = "Tomek", Nazwisko = "Sola"}});
 
-            var node = lista.First;
-
-            while (node != null)
+            pracownicy.Add("Księgowość", new List<Pracownik>() { new Pracownik { Imie = "Monika", Nazwisko = "Wysocka"},
+                                                                            new Pracownik { Imie = "Agata", Nazwisko = "Beton"},
+                                                                            new Pracownik { Imie = "Marta", Nazwisko = "Tutak"},
+                                                                             new Pracownik { Imie = "Renata", Nazwisko = "Popo"} });
+            foreach (var item in pracownicy)
             {
-                Console.WriteLine(node.Value);
-                node = node.Next;
+                Console.WriteLine("Ilość pracowników w dziale {0} wynosi {1}", item.Key, item.Value.Count);
             }
-
             /*  var liczby = new List<int>(10);
               var pojemnosc = -1;
               while(true)
@@ -57,6 +58,68 @@ namespace _2_KolekcjeGeneryczne
             {
                 Console.WriteLine(listaPracownikow[i].Imie + " " + listaPracownikow[i].Nazwisko);
             } */
+        }
+
+        private static void Slownik2()
+        {
+            var pracownicy = new Dictionary<string, List<Pracownik>>();
+            pracownicy.Add("Ksiegowosc", new List<Pracownik>() { new Pracownik { Nazwisko = "Nowak" }, new Pracownik { Nazwisko = "Kowal" }, new Pracownik { Nazwisko = "Pan" } });
+
+            pracownicy["Ksiegowosc"].Add(new Pracownik { Nazwisko = "Nowak" });
+
+            pracownicy.Add("Informatyka", new List<Pracownik>() { new Pracownik { Nazwisko = "Kowalski" }, new Pracownik { Nazwisko = "Bogacki" } });
+
+            foreach (var item in pracownicy)
+            {
+                Console.WriteLine("Dział: " + item.Key);
+
+                foreach (var pracownik in item.Value)
+                {
+                    Console.WriteLine(pracownik.Nazwisko);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Księgowi:");
+            foreach (var item in pracownicy["Ksiegowosc"])
+            {
+                Console.WriteLine(item.Nazwisko);
+            }
+        }
+
+        private static void Slownik()
+        {
+            var pracownicy = new Dictionary<string, Pracownik>();
+            pracownicy.Add("Nowak", new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            pracownicy.Add("Kowal", new Pracownik { Imie = "Marcin", Nazwisko = "Kowal" });
+            pracownicy.Add("Kaczor", new Pracownik { Imie = "Marcin", Nazwisko = "Kaczor" });
+
+            var pracownik = pracownicy["Kowal"];
+
+            foreach (var pracus in pracownicy)
+            {
+                Console.WriteLine("{0}:{1}", pracus.Key, pracus.Value.Imie);
+            }
+        }
+
+        private static void LinkedList2()
+        {
+            LinkedList<int> lista = new LinkedList<int>();
+            lista.AddFirst(5);
+            lista.AddFirst(6);
+            lista.AddFirst(7);
+
+            var element1 = lista.First;
+            var elementOstatni = lista.Last;
+            lista.AddAfter(element1, 10);
+            lista.AddBefore(element1, 20);
+
+            var node = lista.First;
+
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
         }
 
         private static void LinkedList()
---
+++ KolekcjeGeneryczne_TEST/Slownik_TEST.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace KolekcjeGeneryczne_TEST
+{
+    [TestClass]
+    public class Slownik_TEST
+    {
+        [TestMethod]
+        public void SlownikJakoMapa()
+        {
+            var mapa = new Dictionary<int, string>();
+            mapa.Add(1, "jeden");
+            mapa.Add(2, "dwa");
+            mapa.Add(3, "trzy");
+
+            Assert.AreEqual("jeden", mapa[1]);
+        }
+        [TestMethod]
+        public void PrzeszukiwanieSlownika()
+        {
+            var mapa = new Dictionary<int, string>();
+            mapa.Add(1, "jeden");
+            mapa.Add(2, "dwa");
+            mapa.Add(3, "trzy");
+
+            Assert.IsTrue(mapa.ContainsKey(2));
+        }
+        [TestMethod]
+        public void UsuwanieWartosci()
+        {
+            var mapa = new Dictionary<int, string>();
+            mapa.Add(1, "jeden");
+            mapa.Add(2, "dwa");
+            mapa.Add(3, "trzy");
+            mapa.Remove(1);
+
+            Assert.IsFalse(mapa.ContainsKey(1));
+            Assert.AreEqual(2, mapa.Count);
+        }
+        [TestMethod]
+        public void CzyszczenieSlownka()
+        {
+            var mapa = new Dictionary<int, string>();
+            mapa.Add(1, "jeden");
+            mapa.Add(2, "dwa");
+            mapa.Add(3, "trzy");
+            mapa.Clear();
+
+            Assert.AreEqual(0, mapa.Count);
+        }
+        [TestMethod]
+        public void SlownikJakoSlownik()
+        {
+            var mapa = new Dictionary<string, string>();
+            mapa.Add("one", "jeden");
+            mapa.Add("two", "dwa");
+            mapa.Add("three", "trzy");     
+            Assert.AreEqual("jeden", mapa["one"]);
+        }
+    }
+}
